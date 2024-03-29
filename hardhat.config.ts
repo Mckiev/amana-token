dfae1d1c402aa166f562e47fddf991eb3a06103e
Anton Makiievskyi <iammckiev@gmail.com>
2024-03-28 17:20:36 -0700

    added rpc url to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     polygon: {
-      url: "https://polygon.llamarpc.com",
+      url: process.env.POLYGONSCAN_RPC_URL,
       accounts: {
         mnemonic: process.env.TEST_MNEMONIC,
         path: "m/44'/60'/0'/0",
